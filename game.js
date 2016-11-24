1f1fc482027f12b4e49f63129d8274410aac54a5
brandon <86nguyenb@gmail.com>
2016-11-23 21:07:24 -0500

    Initial Commit

--- game.js
+++ game.js
@@ -75,3 +75,35 @@ var POP {
 
 window.addEventListener('load', POP.init, false);
 window.addEventListener('resize', POP.resize, false);
+
+
+
+
+// abstracts various canvas operations into standalone functions
+
+POP.Draw = {
+
+    clear: function() {
+        POP.ctx.clearRect(0, 0, POP.WIDTH, POP.HEIGHT);
+    },
+
+    rect: function(x, y, w, h, col) {
+        POP.ctx.fillStyle = col;
+        POP.ctx.fillRect(x, y, w, h);
+    },
+
+    circle: function(x, y, r, col) {
+        POP.ctx.fillStyle = col;
+        POP.ctx.beginPath();
+        POP.ctx.arc(x + 5, y + 5, r, 0, Math.PI * 2, true);
+        POP.ctx.closePath();
+        POP.ctx.fill();
+    },
+
+    text: function(string, x, y, size, col) {
+        POP.ctx.font = 'bold ' + size + 'px Monospace';
+        POP.ctx.fillStyle = col;
+        POP.ctx.fillText (string, x, y);
+    }
+
+};
